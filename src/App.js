4aabb28225f175ad478a7d41408c7b5f213f9469
Michael Mayes <m0m0sxr@homeoffice.wal-mart.com>
2022-02-11 14:03:09 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState} from "react";
 
-function App() {
+export default function App() {
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
+
+  const getAnswer = (e) => {
+    e.preventDefault();
+    if (!question) {
+      return;
+    }
+    let url = `https://8ball.delegator.com/magic/JSON/${question}`
+    fetch(url)
+      .then(res => res.json())
+      .then(json => {
+        console.log(json.magic)
+        const newArr = [];
+        newArr.push(json.magic)
+        console.log(newArr)
+        setAnswer(newArr[0].answer);
+      })
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={getAnswer}>
+        <div>
+          <label>question</label>
+          <input
+          value={question}
+          onChange={(e) => setQuestion(e.target.value)}
+          />
+        </div>
+        <button type="submit">get answer</button>
+      </form>
+      <div className="circle">
+        <p>{answer}</p>
+      </div>
     </div>
   );
 }
 
-export default App;
